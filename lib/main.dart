73c913527b7c6a14cc5ca66d1427c21a0c914395
Camilo Cardona <acardona@octansit.com>
2022-01-22 18:19:44 -0500

    rutas, screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,20 @@
 import 'package:flutter/material.dart';
 
+import 'package:form_validation_app/screens/screens.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      title: 'Productos App',
+      initialRoute: LoginScreen.routeName,
+      routes: {
+        LoginScreen.routeName: (_) => LoginScreen(),
+        HomeScreen.routeName: (_) => HomeScreen()
+      },
     );
   }
 }
---
+++ lib/screens/screens.dart
@@ -0,0 +1,2 @@
+export 'package:form_validation_app/screens/home_screen.dart';
+export 'package:form_validation_app/screens/login_screen.dart';
